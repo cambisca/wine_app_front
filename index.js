43e3b5da148856440a0be7473b41175fae47dafb
Cameron Bisca <cjbisca@gmail.com>
2021-01-27 14:27:27 -0500

    front commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const currentUser = {
+    id: 1,
+    name: "Cam",
+    username: "Cammy",
+    password: "abc123",
+    age: 21
+}
+
 //********************** DOM selectors *************************
 const wineCard = document.querySelector('#wine-card')
 const wineDetail = document.querySelector('#wine-detail')
 const sideCarousel = document.querySelector('#side-carousel')
 const newWineForm = document.querySelector('#new-wine')
+const favoriteWineList = document.querySelector('#favorites-list')
+const occasionDropdown = document.querySelector('#occasion-dropdown')
 
 getAllWines()
 
@@ -26,6 +36,22 @@ function getSingleWine(id){
     .then(displayWineDetail)
 }
 
+function postWineObj(newWineObj){
+    fetch('http://localhost:3000/wines', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newWineObj),
+    })
+    .then(response => response.json())
+    .then((wine) => {
+        renderNewWine(wine);
+    })
+
+
+}
+
 // ********************* Event Listeners **************************
 sideCarousel.addEventListener('click', evt => {
     if (evt.target.matches("img")){
@@ -35,12 +61,14 @@ sideCarousel.addEventListener('click', evt => {
 
 newWineForm.addEventListener('submit', createNewWine)
 
+
+
 // ******************* DOM Manipulation ********************
 function createWine(wine) {
     const img = document.createElement('img')
     img.src = wine.image_url
     img.alt = wine.name
-    img.dataset.id = wine.id 
+    img.dataset.id = wine.id
     document.querySelector('#side-carousel').append(img)
 }
 
@@ -53,7 +81,7 @@ function displayWineDetail(wineObj) {
     classification.innerText = wineObj.classification
     // year
     const year = document.createElement('p')
-    year.innerText = wineObj.year 
+    year.innerText = wineObj.year
     //varietal
     const varietal = document.createElement('p')
     varietal.innerText = wineObj.varietal
@@ -64,27 +92,90 @@ function displayWineDetail(wineObj) {
     const img = document.createElement('img')
     img.src = wineObj.image_url
     img.alt = wineObj.name
+     // favorite button
+    const favoriteButton = document.createElement('button')
+    favoriteButton.innerHTML = "Favorite"
+
     // slap it on the dom
-    wineDetail.append(img, name, classification, year, varietal, review)
-   
+    wineDetail.append(img, favoriteButton, name, classification, year, varietal, review)
+
+
+    favoriteButton.addEventListener('click', (e) => {
+        fetch(`http://localhost:3000/wines/${wineObj.id}`)
+        .then(res => res.json())
+        .then((wine) => {
+            renderWineLi(wine)
+        })
+    })
+}
+
+function renderNewWine(wine){
+      // name
+      const name = document.createElement('p')
+      name.innerText = wine.name
+      // classification
+      const classification = document.createElement('p')
+      classification.innerText = wine.classification
+      // year
+      const year = document.createElement('p')
+      year.innerText = wine.year
+      //varietal
+      const varietal = document.createElement('p')
+      varietal.innerText = wine.varietal
+      // review
+      const review = document.createElement('p')
+      review.innerText = wine.review
+      // image
+      const img = document.createElement('img')
+      img.src = wine.image_url
+      img.alt = wine.name
+      // slap it on the dom
+      wineDetail.append(img, name, classification, year, varietal, review)
 }
 
 
 
-
-// still need to build out 
+// still need to build out
 function createNewWine(evt) {
     evt.preventDefault()
-    console.log(evt.target)
+
+    console.log(evt.target.classification)
+
+    const newWineObj = {
+        name: evt.target.name.value,
+        classification: evt.target.classification.value,
+        year: evt.target.year.value,
+        varietal: evt.target.varietal.value,
+        review: evt.target.review.value,
+        image_url: evt.target.image_url.value
+    }
+    console.log(newWineObj)
+
+    postWineObj(newWineObj)
 
 }
 
-// function deleteFavorite() {
+function renderWineLi(wine) {
+    const wineFavoriteLi = document.createElement('li')
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = "Delete"
 
-// }
+    wineFavoriteLi.dataset.id = wine.id
+    wineFavoriteLi.innerText = wine.name
+    wineFavoriteLi.name = wine.name
 
-// function updateOccasion() {
-    // occasionDropdown.addEventListener('click', (e) => {
+    wineFavoriteLi.append(deleteButton)
+    favoriteWineList.append(wineFavoriteLi)
 
-    // })
-// }
+    // ---------> Where we left off <----------
+    deleteButton.addEventListener('click', (event) => {
+        
+        fetch(`http://localhost:3000/users/1`)
+        .then(res => res.json())
+        .then(console.log)
+
+    })
+}
+
+
+
